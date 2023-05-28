a85bd6c034da554bdb37e957a438cb35578fda00
RedleoDesigner <info@redleodesign.com>
2023-05-28 19:34:32 +0200

    server url

--- client/script.js
+++ client/script.js
@@ -106,7 +106,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://devflow.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
